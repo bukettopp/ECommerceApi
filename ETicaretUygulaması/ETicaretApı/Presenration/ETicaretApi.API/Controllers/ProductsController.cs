cee8f9647ad0b2fb390844864e110af9f6dcdfaf
bukettopp <157303213+bukettopp@users.noreply.github.com>
2025-06-10 00:56:11 +0300

    Project structure established
    Onion Architecture structure was established

---
+++ ETicaretUygulaması/ETicaretApı/Presenration/ETicaretApi.API/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using ETicaretApi.Application.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretApi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = _productService.GetProducts();
+            return Ok( products );
+        }
+    }
+}
