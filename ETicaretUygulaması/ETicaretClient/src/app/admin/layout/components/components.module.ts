72dff9e475db8d2b0820ce8ba6871bb195d9d57f
bukettopp <157303213+bukettopp@users.noreply.github.com>
2025-07-21 23:12:39 +0300

    Angular Meterial Entegrasyonu

--- ETicaretUygulaması/ETicaretClient/src/app/admin/layout/components/components.module.ts
+++ ETicaretUygulaması/ETicaretClient/src/app/admin/layout/components/components.module.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
-
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
@@ -14,7 +14,8 @@ import { RouterModule } from '@angular/router';
     FooterComponent
   ],
   imports: [
-    CommonModule,RouterModule
+    CommonModule,RouterModule,
+    MatListModule
   ],
   exports :[
     HeaderComponent,
